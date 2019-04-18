2f44caba55f70eed4dbdf585073fbc4cc3aa2efb
Frederico Santos <fred.sant@hotmail.com>
2019-04-18 04:07:28 +0100

    First attempt queries
    Query being made when clicked;
    
    Query executed;
    
    Map not updated;

--- nodejs-express/pg_mapper/routes/index.js
+++ nodejs-express/pg_mapper/routes/index.js
@@ -14,12 +14,12 @@ var conString = "postgres://"+username+":"+password+"@"+host+"/"+database; // Yo
 // Set up your database query to display GeoJSON
 var drawLines = "SELECT row_to_json(fc) FROM (	SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (	SELECT 'Feature' As type, ST_AsGeoJSON(lg.geomline)::json As geometry, row_to_json((taxi_id,data_time)) As properties FROM tracks As lg LIMIT 1) 	As f) As fc ";
 
-/*var drawTracks = "SELECT row_to_json(fc) FROM 
-                    (SELECT 'ID_Track' As type,array_to_json(array_agg(track_list)) As features FROM  
-                      (SELECT 'Trajectoria' As type, array_to_json(array_agg(track_indi)) As features FROM  
-                        (SELECT 'Ponto' As type, ST_AsGeoJSON(lg.geomline)::json As geometry, row_to_json((taxi_id,data_time)) As properties FROM tracks As lg LIMIT 1500000) 
-                      As track_indi) 
-                    As track_list) 
+/*var drawTracks = "SELECT row_to_json(fc) FROM
+                    (SELECT 'ID_Track' As type,array_to_json(array_agg(track_list)) As features FROM
+                      (SELECT 'Trajectoria' As type, array_to_json(array_agg(track_indi)) As features FROM
+                        (SELECT 'Ponto' As type, ST_AsGeoJSON(lg.geomline)::json As geometry, row_to_json((taxi_id,data_time)) As properties FROM tracks As lg LIMIT 1500000)
+                      As track_indi)
+                    As track_list)
                   As fc ";*/
 
 var drawTracks    = "SELECT row_to_json(fc) FROM (SELECT 'Trajectoria' As type, array_to_json(array_agg(track_indi)) As features FROM (SELECT 'P' As type, ST_AsGeoJSON(lg.geomline)::json As geometry, row_to_json((lg.taxi_id,lg.data_time)) As properties FROM (SELECT taxi_id, data_time FROM trajectory_lines GROUP BY taxi_id,data_time ORDER BY taxi_id,data_time) As t JOIN trajectory_lines As lg ON lg.taxi_id = t.taxi_id AND lg.data_time = t.data_time LIMIT 1400000) As track_indi ) As fc ";
@@ -40,13 +40,13 @@ taxi_id,data_time)) AS mylines
 var select_first = "SELECT taxi_id from (SELECT taxi_id,row_number() as rn,count(*) over () as total_countFROM tracks) t where rn = 1 or rn = total_count";
 
 /*var tentativa = "SELECT (
-  SELECT 
-     ID, 
-     ValueV, 
+  SELECT
+     ID,
+     ValueV,
      Keys = JSON_QUERY('["' + STRING_AGG(STRING_ESCAPE(Keys, 'json'), '","') + '"]')
   FOR JSON PATH
 )
-FROM #Test 
+FROM #Test
 GROUP BY ID, ValueV*/
 
 
@@ -59,7 +59,7 @@ GROUP BY ID, ValueV*/
 // 1 degree = 60 * 1852 meters = 111.120 kilometers
 // ST_DWithin geom geom, distance (in units of sgrid)--- should be meters but it aint, 0.0009 == 100meters sort of
 
-var first_try_query = "SELECT row_to_json(fc) FROM (SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((lg.taxi_id,lg.data_time_Start,lg.data_time_End)) As properties FROM trajectory_lines As lg WHERE ST_DWithin(geom,ST_SetSRID(ST_MakePoint(116.46383,39.95497),32650),0.003) LIMIT 5000000) 	As f) As fc"
+var first_try_query = "SELECT row_to_json(fc) FROM (SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((lg.taxi_id,lg.data_time_Start,lg.data_time_End)) As properties FROM trajectory_lines As lg WHERE ST_DWithin(geom,ST_SetSRID(ST_MakePoint(116.46383,39.95497),32650),0.0009) LIMIT 5000000) 	As f) As fc"
 
 
 
@@ -84,7 +84,7 @@ router.get('/data', function (req, res) {
   query.on("row", function (row, result) {
       result.addRow(row);
   });
-  
+
   query.on("end", function (result) {
       var fs = require('fs');
       fs.writeFileSync('./public/data/geoJSON.json', JSON.stringify(result.rows[0].row_to_json));
@@ -96,36 +96,38 @@ router.get('/data', function (req, res) {
 /* GET the map page */
 var timeB4draw = Math.floor( new Date().getTime()/1000);
 router.get('/map', function(req, res) {
-  
+
   var client = new Client(conString); // Setup our Postgres Client
   client.connect(); // connect to the client
-
+  console.log("Fetching on database");
   var query = client.query(new Query(drawTracksMap)); // Run our Query
   query.on("row", function (row, result) {
       result.addRow(row);
   });
-  
+
   var timefetch = Math.floor( new Date().getTime()/1000);
   var timeafterGet = timefetch-timeB4draw;
-  console.log("After get map");
-  console.log(timeafterGet);
+
+
+
   // Pass the result to the map page
   query.on("end", function (result) {
+
+      console.log("Passing data to frontend");
       //var data = require('../public/data/geoJSON.json')
       var data = result.rows[0].row_to_json // Save the JSON as variable data
-     
+
       res.render('map', {
           title: "BigGeo", // Give a title to our page
           jsonData: data // Pass data to the View
       });
-      console.log("DATA PASSED TO BE DRAWN");
       var timeAdraw = Math.floor( new Date().getTime()/1000);
-      console.log(timeAdraw-timefetch);
   });
-  
+
 });
 
-function queryLenses(){
+router.get('/map/:valores', function(req, res) {
+
   var client = new Client(conString); // Setup our Postgres Client
   client.connect(); // connect to the client
 
@@ -133,16 +135,15 @@ function queryLenses(){
   query.on("row", function (row, result) {
       result.addRow(row);
   });
-  
+
   var timefetch = Math.floor( new Date().getTime()/1000);
   var timeafterGet = timefetch-timeB4draw;
-  console.log("After get map");
-  console.log(timeafterGet);
+  console.log("Updating map");
   // Pass the result to the map page
   query.on("end", function (result) {
       //var data = require('../public/data/geoJSON.json')
       var data = result.rows[0].row_to_json // Save the JSON as variable data
-     
+
       res.render('map', {
           title: "BigGeo", // Give a title to our page
           jsonData: data // Pass data to the View
@@ -151,5 +152,5 @@ function queryLenses(){
       var timeAdraw = Math.floor( new Date().getTime()/1000);
       console.log(timeAdraw-timefetch);
   });
-  
-}
+
+});
